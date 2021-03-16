d716fe47189e604ca67b2d398c11493ae4f71278
Ryan Hubble <rhubble1987@gmail.com>
2021-03-16 00:52:51 -0400

    Updated package.json

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { HashRouter as Router, Route } from "react-router-dom";
+import { HashRouter as Router, Route, Link } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./pages/Home";
 import Portfolio from "./pages/Portfolio";
