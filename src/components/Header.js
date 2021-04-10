ab7ffc82c3993a0a950fa8a12dc5ce5b5ff38ac5
rhubble1987 <69992038+rhubble1987@users.noreply.github.com>
2021-04-10 10:16:18 -0400

    Updated resume

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link, useLocation } from "react-router-dom";
-import ResumeFile from "../Assets/PDFs/resume2021-03-12.pdf";
+import ResumeFile from "../Assets/PDFs/ryan_hubble_resume_20210409.pdf";
 
 function Header() {
     const location = useLocation();
